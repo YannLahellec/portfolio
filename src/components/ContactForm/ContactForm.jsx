c84e751d479cfed46d9fc74a9291f0fca7f32270
YannLahellec <lahellec.yann@gmail.com>
2024-07-15 12:32:19 +0200

    css bugs fixed

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -9,18 +9,23 @@ import "./contact.css";
 function ContactForm() {
   const form = useRef();
 
-  console.log(form)
+  console.log(form);
 
   const sendEmail = (e) => {
     e.preventDefault();
-  
+
     emailjs
-      .sendForm(`${import.meta.env.VITE_YOUR_SERVICE_ID}`, `${import.meta.env.VITE_YOUR_TEMPLATE_ID}`, form.current, {
-        publicKey: `${import.meta.env.VITE_YOUR_PUBLIC_KEY}`,
-      })
+      .sendForm(
+        `${import.meta.env.VITE_YOUR_SERVICE_ID}`,
+        `${import.meta.env.VITE_YOUR_TEMPLATE_ID}`,
+        form.current,
+        {
+          publicKey: `${import.meta.env.VITE_YOUR_PUBLIC_KEY}`,
+        }
+      )
       .then(
         () => {
-          toast.success('Message envoyé', {
+          toast.success("Message envoyé", {
             position: "top-right",
             autoClose: 5000,
             hideProgressBar: false,
@@ -29,40 +34,58 @@ function ContactForm() {
             draggable: true,
             progress: undefined,
             theme: "dark",
-            });
+          });
         },
         (error) => {
           console.log("FAILED...", error.text);
         }
       );
+    e.target.reset();
   };
 
   return (
     <section className="contact-section" id="contact">
       <ToastContainer
-      position="top-right"
-      autoClose={5000}
-      hideProgressBar={false}
-      newestOnTop={false}
-      closeOnClick
-      rtl={false}
-      pauseOnFocusLoss
-      draggable
-      pauseOnHover
-      theme="dark"
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
       />
       <h1>CONTACT</h1>
       <form ref={form} onSubmit={sendEmail} className="form-container">
         <label>Name</label>
-        <input type="text" name="user_name" className="input-contact"  placeholder="Nom..." required/>
+        <input
+          type="text"
+          name="user_name"
+          className="input-contact"
+          placeholder="Nom..."
+          required
+        />
         <label>Email</label>
-        <input type="email" name="user_email" className="input-contact" placeholder="...@mail.com" required/>
+        <input
+          type="email"
+          name="user_email"
+          className="input-contact"
+          placeholder="...@mail.com"
+          required
+        />
         <label>Message</label>
-        <textarea name="message" className="input-contact-message" placeholder="Message" required />
-        <input type="submit" value="ENVOYER" className="send-input"/>
+        <textarea
+          name="message"
+          className="input-contact-message"
+          placeholder="Message"
+          required
+        />
+        <input type="submit" value="ENVOYER" className="send-input" />
       </form>
       <section>
-        <div className="contact-icon-container">  
+        <div className="contact-icon-container">
           <img
             src={Linkedin}
             alt="icon"
