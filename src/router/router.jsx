fac901c1cceeb76a3e5dddb861eee7d304e635e0
Méwen DORAFI <dorafimewen@pm.me>
2024-07-27 17:55:44 +0200

    ♻️ refactor router

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,26 +1,20 @@
-import App from "../App";
-import HomePage from "../Pages/HomePage";
-import HobbiesPage from "../Pages/hobbies/HobbiesPage";
-import Pages from '../Pages'
-import { createBrowserRouter } from "react-router-dom";
+import App from '../App';
+import pages from '../Pages';
+import { createBrowserRouter } from 'react-router-dom';
 
-console.log(Pages);
+const routes = Object.keys(pages).map(page => {
+	const Component = pages[page];
+	const path = page.toLowerCase().replace(/page/, '');
+	return {
+		path: `/${path !== 'home' ? path : ''}`,
+		element: <Component />,
+	};
+});
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        path: "/",
-        element: <HomePage />,
-      },
-      {
-        path: "/hobbies",
-        element: <HobbiesPage/>
-      }
-    ],
-  },
+export default createBrowserRouter([
+	{
+		path: '/',
+		element: <App />,
+		children: routes,
+	},
 ]);
-
-export default router;
